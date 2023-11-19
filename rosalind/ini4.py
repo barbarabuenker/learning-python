1766c85a569d31f93c37b093bbe1719433702ebd
Barbara Buenker <barbara.buenker@chello.at>
2023-11-19 19:37:49 +0100

    Added solution

--- rosalind/ini4.py
+++ rosalind/ini4.py
@@ -1,2 +1,18 @@
 # Rosalind INI4: Conditions and Loops
+## Given: Two positive integers a and b (a<b<10000).
+## Return: The sum of all odd integers from through b, inclusively.
+
+a = 4468
+b = 9027
+
+result = 0
+for n in range(a,b+1):
+    if n % 2 == 1:
+        result = result + n
+    else:
+        continue
+
+print(result)
+
+## My result: 15385440 (correct!)
 
